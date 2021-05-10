ebfcf97a7e2ec44312fb2c978021c135db12ac45
1234borkowskip <46341908+1234borkowskip@users.noreply.github.com>
2021-05-10 22:04:44 +0200

    Update main.py

--- zad2/src/main.py
+++ zad2/src/main.py
@@ -1,27 +1,6 @@
 import csv
 import math
 
-def find_minimal(data=None):
-    minimal = None
-    for row in data:
-        val = int(row[0])
-        if not minimal:
-            minimal = val
-        elif val < minimal:
-            minimal = val
-    return minimal
-
-
-def find_maximal(data=None):
-    minimal = None
-    for row in data:
-        val = int(row[0])
-        if not minimal:
-            minimal = val
-        elif val > minimal:
-            minimal = val
-    return minimal
-
 
 def normalize(data=None, index=None):
     row = [i for i in zip(*data)][index]
@@ -30,18 +9,53 @@ def normalize(data=None, index=None):
     max_row = max(row)
     return (float(row[index]) - min_row) / (max_row - min_row)
 
+def get_min_and_max(data=None, index=None):
+    row = [i for i in zip(*data)][index]
+    row = [float(i) for i in row]
+
+    return min(row), max(row)
+
 def euclidean(row1, row2):
     distance = 0.0
-    for i in range(len(row1)-1):
+    for i in range(len(row1)):
         distance += (row1[i] - row2[i])**2
+
     return math.sqrt(distance)
 
 def manhattan(row1, row2):
     distance = 0.0
-    for i in range(len(row1)-1):
+    for i in range(len(row1)):
         distance = abs(row1[i] - row2[i])
+
     return distance
 
+def logarithm(row1, row2):
+    distance = 0.0
+    for i in range(len(row1)):
+        if row1[i] <= 0:
+            value1 = 0
+        else:
+            value1 = math.log(row1[i])
+        if row2[i] <= 0:
+            value2 = 0
+        else:
+            value2 = math.log(row1[i])
+        distance = abs(value1-value2)
+
+    return distance
+
+def czebyszew(row1, row2):
+    distance = 0.0
+    for i in range(len(row1)):
+        distance = max(abs(row1[i] - row2[i]))
+
+    return distance
+
+def minkowski(row1, row2, p):
+    distance = 0.0
+    for i in range(len(row1)):
+        distance = ((abs(row1[i] - row2[i])**p))**(1/p)
+    return distance
 
 with open('datasets/iris.txt') as f:
     reader = csv.reader(f, delimiter='	')
@@ -63,13 +77,51 @@ normalized_iris = []
 class_index = [4]
 for row in iris:
     normalized_row = []
-    for index, _ in enumerate(row):
+    for index, value in enumerate(row):
         if index in class_index:
-            normalized_row.append(iris_class[index])
+            normalized_row.append(value)
             continue
-        normalized = normalize(data=iris, index=index)
+        _min, _max = get_min_and_max(data=iris, index=index)
+        normalized = (float(value) - _min) / (_max - _min)
         normalized_row.append(normalized)
     normalized_iris.append(normalized_row)
 
+testrow = [1, 1, 1, 1]
+distances = []
+for row in normalized_iris:
+    distance_row = []
+    for index, value in enumerate(row):
+        if index in class_index:
+            distance_row.append(value)
+            continue
+    distance = euclidean(testrow, row)
+    distance_row.append(distance)
+    distances.append(distance_row)
+
+for row in distances:
+    print(row)
+
+k = 3
+
+
+def knn1(distances, k):
+    knn=[]
+    i=0
+    distances.sort(key=lambda value: value[1])
+    while i < k:
+        for row in distances:
+            knn.append(row)
+            i += 1
+    return knn
+
+def knn2(distances, k):
+    knn = []
+    distances.sort(key=lambda value: value[1])
+
+knn = knn1(distances, k)
+
 # for row in normalized_iris:
+#     print(row)
+
+# for row in knn:
 #     print(row)
